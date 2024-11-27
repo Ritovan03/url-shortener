8213c51f4eeca96469482acbfcc367b59e3d38a9
Ritovan <147165729+RitovanDasgupta@users.noreply.github.com>
2024-11-27 12:09:21 +0530

    first commit

---
+++ controllers/url.js
@@ -0,0 +1,46 @@
+
+const shortId = require('shortid');
+const URL = require('../models/url');
+
+async function handleGenerateNewShortURL(req, res) {
+    const shortID = shortId();
+    const body = req.body;
+    if(!body.url) return res.status(400).json({error :'URL is required'});
+    const newURL = new URL({
+        shortId: shortID,
+        redirectURL: body.url,
+        visitHistory: [],
+    });
+
+    try {
+        await newURL.save(); // Save the new URL to the database
+        return res.status(201).json({ id: shortID }); // Respond with the generated short ID
+    } catch (error) {
+        console.error("Error saving URL:", error.message);
+        return res.status(500).json({ error: "Internal Server Error" });
+    }
+
+}
+
+async function handleGetAnalytics(req,res) {
+    const shortID = req.params.shortId;
+    const result = await URL.findOne({shortID});
+    return res.json({
+        totalClicks:result.visitHistory.length,
+        analytics:result.visitHistory
+    });
+    
+}
+
+async function handleGetAnalytics(req, res) {  
+    const shortID = req.params.shortID;
+    const result = await URL.findOne({shortID});
+    return res.json({
+        totalClicks:result.visitHistory.length,analytics:result.visitHistory,
+    });
+}
+
+module.exports = {
+    handleGenerateNewShortURL,
+    handleGetAnalytics
+}
---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+const PORT = 8080;
+const urlRoute = require('./routes/url');
+const {connectToMongoDB} = require('./connect');
+const URL = require('./models/url');
+
+
+connectToMongoDB('mongodb://localhost:27017/short-url')
+.then(()=> console.log("MongoDB connected"));
+
+app.use(express.json());
+
+app.get('/:shortId', async (req, res) => {
+    const shortId = req.params.shortId;
+    const entry = await URL.findOneAndUpdate({
+        shortId: shortId
+    }, 
+    {$push: {visitHistory: {
+        timestamp: Date.now()
+    },
+
+    }});
+    res.redirect(entry.redirectURL);
+});
+
+
+
+
+app.listen(PORT, () => console.log(`server running on port ${PORT}`));
+
+app.use("/url",urlRoute);
