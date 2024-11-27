ae029c813a19d65ed238f024845c475a51a6783f
Ritovan <147165729+RitovanDasgupta@users.noreply.github.com>
2024-11-27 15:27:24 +0530

    added ssr

--- index.js
+++ index.js
@@ -4,16 +4,21 @@ const PORT = 8080;
 const urlRoute = require('./routes/url');
 const {connectToMongoDB} = require('./connect');
 const URL = require('./models/url');
-
+const path = require('path');
+const staticRoute = require('./routes/staticRouter');
 
 connectToMongoDB('mongodb://localhost:27017/short-url')
 .then(()=> console.log("MongoDB connected"));
 
 app.set('view engine', 'ejs');
+app.set('views', path.resolve("./views/"));
 
 app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
-app.get('/:shortId', async (req, res) => {
+app.use("/",staticRoute);
+
+app.get('/url/:shortId', async (req, res) => {
     const shortId = req.params.shortId;
     const entry = await URL.findOneAndUpdate({
         shortId: shortId
