b0cc67b5f533ef76ef4b375e1e3b0b514db817e7
Ritovan <147165729+RitovanDasgupta@users.noreply.github.com>
2024-12-09 12:49:22 +0530

    user login

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,11 @@
 //for auth
 
 const express = require("express");
-const {handleUserSignup} = require("../controllers/user");
+const {handleUserSignup,handleUserLogin} = require("../controllers/user");
 const router = express.Router();
 
 router.post('/',handleUserSignup);
+
+router.post("/login",handleUserLogin);
 
 module.exports = router;
