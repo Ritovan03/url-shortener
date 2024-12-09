a3cabba23feb77938f03927f15c4b7e9dcd572f5
Ritovan <147165729+RitovanDasgupta@users.noreply.github.com>
2024-12-09 12:56:26 +0530

    implementing cookies

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,5 @@
 const User = require("../models/user");
-
+const {v4: uuidv4} = require('uuid');
 
 async function handleUserSignup(req, res) {
     const { name, email, password } = req.body;
@@ -23,6 +23,8 @@ async function handleUserLogin(req, res) {
             {error:"Invalid Credentials"}
         );
     }
+
+    const sessionId = uuidv4();
         
     return res.redirect("/");
 }
